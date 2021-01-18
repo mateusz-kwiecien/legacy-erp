c86127fd1512cd45406f1abbf1009b5c1fc369b3
mkwiecien <mateusz.kwiecien@onwelo.com>
2021-01-18 09:20:10 +0100

    Project structure initializated, with CircleCI.

---
+++ src/test/java/pl/mkwiecien/legacyerp/LegacyErpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mkwiecien.legacyerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegacyErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
