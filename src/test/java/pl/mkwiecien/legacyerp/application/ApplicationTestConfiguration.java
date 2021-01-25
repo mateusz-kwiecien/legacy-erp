6579c91d5dfebc02ceaf90be60a6451f7b7c7617
mkwiecien <mateusz.kwiecien@onwelo.com>
2021-01-25 14:37:33 +0100

    CreateEmployee controller and EmployeeList controller added.

--- src/test/java/pl/mkwiecien/legacyerp/application/ApplicationTestConfiguration.java
+++ src/test/java/pl/mkwiecien/legacyerp/application/ApplicationTestConfiguration.java
@@ -1,18 +1,9 @@
 package pl.mkwiecien.legacyerp.application;
 
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.test.context.ActiveProfiles;
 
 @Configuration
 @ComponentScan(basePackages = {"pl.mkwiecien.legacyerp"})
-//@EnableJpaRepositories(value = {"pl.mkwiecien.legacyerp.domain"})
-//@EntityScan(value = {"pl.mkwiecien.legacyerp.domain"})
-//@ActiveProfiles(value = {"test"})
 public class ApplicationTestConfiguration {
 }
---
+++ src/test/java/pl/mkwiecien/legacyerp/domain/employee/controller/CreateEmployeeControllerTest.java
@@ -0,0 +1,56 @@
+package pl.mkwiecien.legacyerp.domain.employee.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import pl.mkwiecien.legacyerp.application.ApplicationTestConfiguration;
+import pl.mkwiecien.legacyerp.domain.employee.repository.EmployeeRepository;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = {ApplicationTestConfiguration.class})
+class CreateEmployeeControllerTest {
+    private static final String NEW_EMPLOYEE_URI = "/employee/new";
+    private static final String FIRST_NAME = "firstName";
+    private static final String LAST_NAME = "lastName";
+    private static final String EMAIL = "example@example.com";
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
+    @Test
+    void shouldAddNewEmployeeFromRequestAndSaveInRepository() throws Exception {
+        // when :
+        ResultActions result = mockMvc.perform(post(NEW_EMPLOYEE_URI)
+                .param("firstName", FIRST_NAME)
+                .param("lastName", LAST_NAME)
+                .param("email", EMAIL));
+
+        // than :
+        result.andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+        Assertions.assertEquals(1, employeeRepository.count());
+    }
+
+    @BeforeEach
+    void setup() {
+        cleanup();
+    }
+
+    @AfterEach
+    void cleanup() {
+        employeeRepository.deleteAll();
+    }
+}
